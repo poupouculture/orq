b87354a5b29ab76f3ab4facc7aad2dc7e87cea9d
yuanyxh <15766118362@139.com>
2022-12-13 22:09:53 +0800

    dashboard init

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashboardPage'
+}
+</script>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
