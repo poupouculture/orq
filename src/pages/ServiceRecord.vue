45e51581146c73ba5ddc5720e56dbaac1ba148b5
mac <chuankengchou@gmail.com>
2022-12-14 14:23:58 +0800

    ^

---
+++ src/pages/ServiceRecord.vue
@@ -0,0 +1,17 @@
+<template>
+    <div style="display: flex;justify-content: center; align-items: center; align-self: center;margin-top: 275px;">
+     <p>
+      SERVICE RECORD
+     </p>
+    </div>
+   </template>
+
+<script>
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ServiceRecord'
+})
+
+</script>
