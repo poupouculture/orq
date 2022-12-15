2a9c1dc2816d638ffeeea1351288fc16d779447c
Taha <taha.khalid227@gmail.com>
2022-12-15 15:38:30 +0500

    navigation of side bar along with quasar table

--- src/pages/ContactSettings.vue
+++ src/pages/ContactSettings.vue
@@ -1,18 +1,17 @@
 <template>
   <div style="display: flex;justify-content: center; align-items: center; align-self: center;margin-top: 275px;">
     <p>
-     My DASHBOARD
+      CONTACT SETTINGS
     </p>
-   </div>
-
-  </template>
+  </div>
+</template>
 
 <script>
 
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'DashBoard'
+  name: 'ContactSettings'
 })
 
 </script>
--- src/pages/Stow.vue
+++ src/pages/Stow.vue
@@ -1,18 +1,17 @@
 <template>
   <div style="display: flex;justify-content: center; align-items: center; align-self: center;margin-top: 275px;">
     <p>
-     My DASHBOARD
+      STOW
     </p>
-   </div>
-
-  </template>
+  </div>
+</template>
 
 <script>
 
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'DashBoard'
+  name: 'StowPage'
 })
 
 </script>
