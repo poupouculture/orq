a61bace3f9187187488faa1ca7e290ac599b9b9a
yuanyxh <15766118362@139.com>
2022-12-16 14:06:04 +0800

    serviceRecord version-1

---
+++ src/components/InputForm.vue
@@ -0,0 +1,71 @@
+<template>
+<div class="input-form my-4 py-5 h-20 flex no-warp justify-between relative">
+    <div class="flex space-x-2">
+      <q-input label="Fuzzy search" class="w-52 rounded-sm" outlined v-model="text" type="search">
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+      <q-input label="Create Time" label-color="black" class="w-52 rounded-sm" outlined v-model="text" type="search">
+        <template v-slot:append>
+          <q-icon name="event" />
+        </template>
+      </q-input>
+
+      <q-select class="w-52 rounded-sm" outlined v-model="selected[0]" :options="options[0]">
+      </q-select>
+
+      <q-select class="w-52 rounded-sm" outlined v-model="selected[1]" :options="options[1]">
+      </q-select>
+
+      <q-select class="w-52 rounded-sm" outlined v-model="selected[2]" :options="options[2]">
+      </q-select>
+    </div>
+
+    <q-btn color="primary" style="border-radius: 4px;" icon="fa-solid fa-plus" label="Add" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputFrom'
+}
+</script>
+
+<script setup>
+import { ref } from 'vue'
+
+const text = ref(''),
+  options = ref([['Status'], ['Staff'], ['Customer']]),
+  selected = ref([['Status'], ['Staff'], ['Customer']])
+</script>
+
+<style lang="scss" scoped>
+.input-form {
+  :deep {
+    .q-field__control,
+    .q-field__marginal {
+      height: 44px;
+      min-height: 44px;
+    }
+
+    .q-field__native {
+      min-height: 44px;
+      color: black;
+    }
+
+    .q-field__label {
+      top: 12px;
+    }
+
+    .q-icon {
+      font-size: 1.2rem;
+      font-weight: 400;
+    }
+    .q-btn {
+      text-transform: none;
+    }
+  }
+}
+</style>
---
+++ src/components/MessageBox.vue
@@ -0,0 +1,61 @@
+<template>
+  <Transition name="message">
+    <div v-if="show" class="bg-grey-4 px-10 py-4 mt-5 rounded-md md:flex no-warp items-center relative">
+      <div class="w-40 m-auto h-24 bg-blue-500"></div>
+      <div class="ml-8 flex-1 h-full sm:mt-10 md:mt-0">
+        <h3 class="leading-none sm:text-left text-lg font-semibold" style="color: #333;">{{ title }}</h3>
+        <p class="mt-6 mr-10 sm:text-left text-sm text-gray-500">{{ message }}</p>
+      </div>
+      <button @click="emit('click', false)" class="cursor-pointer absolute right-8 text-gray-500 text-lg top-5">x</button>
+    </div>
+  </Transition>
+</template>
+
+<script>
+
+export default {
+  name: 'MessageBox'
+}
+</script>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+defineProps({
+  title: String,
+  message: String,
+  show: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const emit = defineEmits(['click'])
+
+</script>
+
+<style lang="scss" scoped>
+.message-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.message-enter-to {
+  opacity: 1;
+  transform: scale(1);
+}
+
+.message-leave-from {
+  opacity: 1;
+  transform: scale(1);
+}
+
+.message-leave-to {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.message-enter-active, .message-leave-active {
+  transition: all .4s ease;
+}
+</style>
