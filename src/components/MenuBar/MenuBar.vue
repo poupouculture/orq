0f3227e9e3d20e49af7510515a8bd1177e96e996
Nusendra Hanggarawan <admin@nusendra.com>
2022-12-30 03:38:31 +0700

    fix: move avatar profile to layout's header

--- src/components/MenuBar/MenuBar.vue
+++ src/components/MenuBar/MenuBar.vue
@@ -1,55 +1,57 @@
 <template>
   <div class="menu-holder">
     <img src="../../assets/images/pluscomp.png" class="plus-img" />
-    <img src="../../assets/images/messages.png" class="message-img" /><img src="../../assets/images/notification.png"
-      class="notification-img" /><img src="../../assets/images/profileavatar.png" class="profile-img" /><img
-      src="../../assets/images/dropdownreal.png" class="dropdown-img" />
+    <img src="../../assets/images/messages.png" class="message-img" /><img
+      src="../../assets/images/notification.png"
+      class="notification-img"
+    /><img
+      src="../../assets/images/profileavatar.png"
+      class="profile-img"
+    /><img src="../../assets/images/dropdownreal.png" class="dropdown-img" />
   </div>
 </template>
 
 <script>
-
-import { defineComponent, ref } from 'vue'
-import './MenuBar.scss'
+import { defineComponent, ref } from "vue";
+import "./MenuBar.scss";
 export default defineComponent({
-  setup () {
+  setup() {
     return {
       drawer: ref(true),
-      menuList
-    }
+      menuList,
+    };
   },
 
-  name: 'MenuBar',
+  name: "MenuBar",
 
-  components: {}
-})
+  components: {},
+});
 const menuList = [
   {
-    icon: 'receipt',
-    label: 'receipt',
-    separator: false
+    icon: "receipt",
+    label: "receipt",
+    separator: false,
   },
 
   {
-    icon: 'receipt',
-    label: 'receipt',
-    separator: false
+    icon: "receipt",
+    label: "receipt",
+    separator: false,
   },
   {
-    icon: 'receipt',
-    label: 'receipt',
-    separator: false
+    icon: "receipt",
+    label: "receipt",
+    separator: false,
   },
   {
-    icon: 'receipt',
-    label: 'receipt',
-    separator: false
+    icon: "receipt",
+    label: "receipt",
+    separator: false,
   },
   {
-    icon: 'receipt',
-    label: 'receipt',
-    separator: false
-  }
-]
-
+    icon: "receipt",
+    label: "receipt",
+    separator: false,
+  },
+];
 </script>
