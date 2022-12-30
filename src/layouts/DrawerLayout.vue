a68d6c46c5a43504d66da99b9006a107363a357e
Nusendra Hanggarawan <admin@nusendra.com>
2022-12-30 21:15:29 +0700

    fix: incorrect style structure in sidebar menu

--- src/layouts/DrawerLayout.vue
+++ src/layouts/DrawerLayout.vue
@@ -21,18 +21,43 @@
               class="input"
             />
           </div>
+        </div>
 
-          <router-link
-            v-for="(menu, index) in menus"
-            :key="index"
-            :to="menu.url"
-            style="text-decoration: none; color: inherit"
-          >
-            <div class="icon-text">
-              <img :src="menu.icon" />
-              <p class="drawer-text">{{ menu.pages_id.name }}</p>
-            </div>
-          </router-link>
+        <div v-for="(menu, index) in menus" :key="index">
+          <q-list bordered v-if="menu.hasSubMenu" class="q-pa-md">
+            <q-expansion-item
+              expand-separator
+              icon="perm_identity"
+              :label="menu.pages_id.name"
+              style="color: #fff"
+            >
+              <div v-for="(child, cIndex) in menu.children" :key="cIndex">
+                <router-link
+                  v-if="child.isMenu"
+                  :to="child.url"
+                  style="text-decoration: none; color: inherit"
+                >
+                  <div class="expanded-content">
+                    <img />
+                    <p class="drawer-text">{{ child.label }}</p>
+                  </div>
+                </router-link>
+              </div>
+            </q-expansion-item>
+          </q-list>
+          <q-list v-else>
+            <q-item>
+              <router-link
+                :to="menu.url"
+                style="text-decoration: none; color: inherit"
+              >
+                <div class="icon-text">
+                  <img :src="menu.icon" />
+                  <p class="drawer-text">{{ menu.pages_id.name }}</p>
+                </div>
+              </router-link>
+            </q-item>
+          </q-list>
         </div>
       </q-scroll-area>
     </q-drawer>
@@ -57,6 +82,8 @@ const menus = computed(() => {
       page.url = menu?.url;
       page.isMenu = menu?.isMenu;
       page.icon = menu?.icon;
+      page.hasSubMenu = menu?.hasSubMenu;
+      page.children = menu?.children;
       return page;
     })
     .filter((page) => {
