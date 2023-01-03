e9abfa1a6c46a430faac8aaed04f51ed6ab4462f
Nusendra Hanggarawan <admin@nusendra.com>
2023-01-03 07:46:52 +0700

    refactor: cleanup OtherInformation component

--- src/components/OtherInformation/OtherInformation.vue
+++ src/components/OtherInformation/OtherInformation.vue
@@ -2,49 +2,31 @@
   <div class="main-hold">
     <img src="../../assets/images/otherinfomain.png" />
     <p class="bottom-textmain">There are currently no custom fields</p>
-    <p class="bottom-textsub">Administrators can go to the system administration and add custom fields to help companies
-      expand their information.</p>
+    <p class="bottom-textsub">
+      Administrators can go to the system administration and add custom fields
+      to help companies expand their information.
+    </p>
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref } from "vue";
+import "./OtherInformation.scss";
 
-import { defineComponent, ref } from 'vue'
-import './OtherInformation.scss'
-export default defineComponent({
-  setup () {
-    return {
-      drawer: ref(true),
-      firstname: ref(''),
-      lastname: ref(''),
-      ID: ref(''),
-      customercode: ref(''),
-      gender: ref(),
-
-      options: [
-        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
-      ],
-      genderdata: ['Male', 'Female', 'Others'],
-
-      tags: ref('')
-    }
-  },
-  data () {
-    return {
-      text: '',
-      date: ''
-    }
-  },
-  name: 'OtherInformation',
-
-  components: {}
-})
-
+const drawer = ref(true);
+const firstname = ref("");
+const lastname = ref("");
+const ID = ref("");
+const customercode = ref("");
+const gender = ref();
+const options = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
+const genderdata = ["Male", "Female", "Others"];
+const tags = ref("");
 </script>
+
 <style lang="sass" scoped>
 .row > div
   padding: 5px 10px
 .row + .row
   margin-top: 1rem
-
 </style>
