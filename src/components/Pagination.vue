4bb471d09bdfe1aed76c55ca32d8f2fe2ac743a9
Nusendra Hanggarawan <admin@nusendra.com>
2023-01-03 11:16:03 +0700

    fix: remove unused definedProps and defineEmits import

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -27,7 +27,7 @@ export default {
 </script>
 
 <script setup>
-import { defineProps, computed } from 'vue'
+import { computed } from 'vue'
 
 const props = defineProps({
   current /* current page */: Number,
