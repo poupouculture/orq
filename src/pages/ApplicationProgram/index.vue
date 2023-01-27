b509381f2e418aaaf8b23066be55fb3f6b5678d7
salimarizi <salim9asmp1pdl@gmail.com>
2023-01-28 06:28:52 +0700

    fix: fixing edit page integration

--- src/pages/ApplicationProgram/index.vue
+++ src/pages/ApplicationProgram/index.vue
@@ -26,99 +26,107 @@
         <q-btn icon="delete" no-caps rounded label="Archive" />
       </div>
     </div>
-    <div class="main-content">
-      <q-table
-        selection="multiple"
-        :rows="data.applicationPrograms"
-        :columns="headerColumns"
-        v-model:pagination="pagination"
-        :loading="loading"
-        v-model:selected="selected"
-        flat
-      >
-        <q-tr :props="props">
-          <q-th
-            v-for="col in props.cols"
-            :key="col.name"
-            :props="props"
-            auto-width
-          >
-            {{ col.label }}
-          </q-th>
-        </q-tr>
-        <template #body-cell-name="props">
-          <q-td :props="props">
-            {{ props.row.name }}
-          </q-td>
-        </template>
-        <template #body-cell-status="props">
-          <q-td :props="props">
-            <span
-              class="text-xs font-semibold py-1 px-2 rounded text-[#70CC6B] bg-green-200"
+    <div class="main-content flex">
+      <div class="w-3/12 pr-4 flex flex-col gap-4">
+        <SubmenuFilter />
+      </div>
+      <div class="w-9/12">
+        <q-table
+          selection="multiple"
+          :rows="data.applicationPrograms"
+          :columns="headerColumns"
+          v-model:pagination="pagination"
+          :loading="loading"
+          v-model:selected="selected"
+          flat
+        >
+          <q-tr :props="props">
+            <q-th
+              v-for="col in props.cols"
+              :key="col.name"
+              :props="props"
+              auto-width
             >
-              {{ props.row.status }}
-            </span>
-          </q-td>
-        </template>
-        <template #body-cell-language="props">
-          <q-td :props="props">
-            {{ props.row.language }}
-          </q-td>
-        </template>
-        <template #body-cell-delivered="props">
-          <q-td :props="props">
-            {{ props.row.delivered ? props.row.delivered : 0 }}
-          </q-td>
-        </template>
-        <template #body-cell-read="props">
-          <q-td :props="props">
-            {{ props.row.read ? props.row.read : 0 }}
-          </q-td>
-        </template>
-        <template #body-cell-replied="props">
-          <q-td :props="props">
-            {{ props.row.replied ? props.row.replied : 0 }}
-          </q-td>
-        </template>
-        <template #body-cell-created_by="props">
-          <q-td :props="props">
-            {{ props.row.user_created }}
-          </q-td>
-        </template>
-        <template #body-cell-created_on="props">
-          <q-td :props="props">
-            {{ props.row.date_created }}
-          </q-td>
-        </template>
-        <template #body-cell-action="props">
-          <q-td :props="props">
-            <router-link
-              :to="`/application-program/${props.row.id}`"
-              style="text-decoration: none; color: inherit"
+              {{ col.label }}
+            </q-th>
+          </q-tr>
+          <template #body-cell-name="props">
+            <q-td :props="props">
+              {{ props.row.name }}
+            </q-td>
+          </template>
+          <template #body-cell-status="props">
+            <q-td :props="props">
+              <span
+                class="text-xs font-semibold py-1 px-2 rounded text-[#70CC6B] bg-green-200"
+              >
+                {{ props.row.status }}
+              </span>
+            </q-td>
+          </template>
+          <template #body-cell-language="props">
+            <q-td :props="props">
+              {{ props.row.language }}
+            </q-td>
+          </template>
+          <template #body-cell-delivered="props">
+            <q-td :props="props">
+              {{ props.row.delivered ? props.row.delivered : 0 }}
+            </q-td>
+          </template>
+          <template #body-cell-read="props">
+            <q-td :props="props">
+              {{ props.row.read ? props.row.read : 0 }}
+            </q-td>
+          </template>
+          <template #body-cell-replied="props">
+            <q-td :props="props">
+              {{ props.row.replied ? props.row.replied : 0 }}
+            </q-td>
+          </template>
+          <template #body-cell-created_by="props">
+            <q-td :props="props">
+              {{ props.row.user_created }}
+            </q-td>
+          </template>
+          <template #body-cell-created_on="props">
+            <q-td :props="props">
+              {{ props.row.date_created }}
+            </q-td>
+          </template>
+          <template #body-cell-action="props">
+            <q-td :props="props">
+              <router-link
+                :to="`/application-program/${props.row.id}`"
+                style="text-decoration: none; color: inherit"
+              >
+                <p class="edit-button">Edit</p>
+              </router-link>
+            </q-td>
+          </template>
+          <template v-slot:bottom>
+            <div
+              class="row justify-between q-pt-lg q-pb-lg"
+              style="width: 100%"
             >
-              <p class="edit-button">Edit</p>
-            </router-link>
-          </q-td>
-        </template>
-        <template v-slot:bottom>
-          <div class="row justify-between q-pt-lg q-pb-lg" style="width: 100%">
-            <div class="col">
-              {{ getPaginationLabel() }}
+              <div class="col">
+                {{ getPaginationLabel() }}
+              </div>
+              <div class="col absolute-bottom-right q-ma-lg">
+                <q-pagination
+                  v-model="page"
+                  @update:model-value="changePage"
+                  :max="totalPage()"
+                  direction-links
+                  flat
+                  color="grey"
+                  active-color="primary"
+                />
+              </div>
             </div>
-            <div class="col absolute-bottom-right q-ma-lg">
-              <q-pagination
-                v-model="page"
-                @update:model-value="changePage"
-                :max="totalPage()"
-                direction-links
-                flat
-                color="grey"
-                active-color="primary"
-              />
-            </div>
-          </div>
-        </template>
-      </q-table>
+          </template>
+        </q-table>
+      </div>
     </div>
   </div>
 </template>
@@ -127,6 +135,8 @@
 import { getApplicationPrograms } from "src/api/aplicationPrograms";
 import { ref, reactive, onMounted, toRefs } from "vue";
 import { useRouter } from "vue-router";
+
+import SubmenuFilter from "../../components/ApplicationProgram/SubmenuFilter.vue";
 
 const router = useRouter();
 
