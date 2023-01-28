13f857cac5014a66b28c8076b2bcb86b2b18d224
Nusendra Hanggarawan <admin@nusendra.com>
2023-01-28 15:15:59 +0700

    fix: conflicts

--- src/pages/ApplicationProgram/index.vue
+++ src/pages/ApplicationProgram/index.vue
@@ -56,67 +56,45 @@
             <span
               class="text-xs font-semibold py-1 px-2 rounded text-[#70CC6B] bg-green-200"
             >
-              {{ col.label }}
-            </q-th>
-          </q-tr>
-          <template #body-cell-name="props">
-            <q-td :props="props">
-              {{ props.row.name }}
-            </q-td>
-          </template>
-          <template #body-cell-status="props">
-            <q-td :props="props">
-              <span
-                class="text-xs font-semibold py-1 px-2 rounded text-[#70CC6B] bg-green-200"
-              >
-                {{ props.row.status }}
-              </span>
-            </q-td>
-          </template>
-          <template #body-cell-language="props">
-            <q-td :props="props">
-              {{ props.row.language }}
-            </q-td>
-          </template>
-          <template #body-cell-delivered="props">
-            <q-td :props="props">
-              {{ props.row.delivered ? props.row.delivered : 0 }}
-            </q-td>
-          </template>
-          <template #body-cell-read="props">
-            <q-td :props="props">
-              {{ props.row.read ? props.row.read : 0 }}
-            </q-td>
-          </template>
-          <template #body-cell-replied="props">
-            <q-td :props="props">
-              {{ props.row.replied ? props.row.replied : 0 }}
-            </q-td>
-          </template>
-          <template #body-cell-created_by="props">
-            <q-td :props="props">
-              {{ props.row.user_created }}
-            </q-td>
-          </template>
-          <template #body-cell-created_on="props">
-            <q-td :props="props">
-              {{ props.row.date_created }}
-            </q-td>
-          </template>
-          <template #body-cell-action="props">
-            <q-td :props="props">
-              <router-link
-                :to="`/application-program/${props.row.id}`"
-                style="text-decoration: none; color: inherit"
-              >
-                <p class="edit-button">Edit</p>
-              </router-link>
-            </q-td>
-          </template>
-          <template v-slot:bottom>
-            <div
-              class="row justify-between q-pt-lg q-pb-lg"
-              style="width: 100%"
+              {{ props.row.status }}
+            </span>
+          </q-td>
+        </template>
+        <template #body-cell-language="props">
+          <q-td :props="props">
+            {{ props.row.language }}
+          </q-td>
+        </template>
+        <template #body-cell-delivered="props">
+          <q-td :props="props">
+            {{ props.row.delivered ? props.row.delivered : 0 }}
+          </q-td>
+        </template>
+        <template #body-cell-read="props">
+          <q-td :props="props">
+            {{ props.row.read ? props.row.read : 0 }}
+          </q-td>
+        </template>
+        <template #body-cell-replied="props">
+          <q-td :props="props">
+            {{ props.row.replied ? props.row.replied : 0 }}
+          </q-td>
+        </template>
+        <template #body-cell-created_by="props">
+          <q-td :props="props">
+            {{ props.row.user_created }}
+          </q-td>
+        </template>
+        <template #body-cell-created_on="props">
+          <q-td :props="props">
+            {{ props.row.date_created }}
+          </q-td>
+        </template>
+        <template #body-cell-action="props">
+          <q-td :props="props">
+            <router-link
+              :to="`/application-program/${props.row.id}`"
+              style="text-decoration: none; color: inherit"
             >
               <p class="edit-button">Edit</p>
             </router-link>
@@ -132,11 +110,7 @@ import { getApplicationPrograms } from "src/api/aplicationPrograms";
 import { ref, reactive, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import BaseTable from "src/components/BaseTable.vue";
-
-import SubmenuFilter from "../../components/ApplicationProgram/SubmenuFilter.vue";
-
 const router = useRouter();
-
 const headerColumns = [
   {
     name: "name",
@@ -205,7 +179,6 @@ const headerColumns = [
     classes: "text-blue",
   },
 ];
-
 const loading = ref(true);
 const data = reactive({
   applicationPrograms: [],
@@ -213,11 +186,9 @@ const data = reactive({
   page: 1,
   rowsPerPage: 10,
 });
-
 onMounted(() => {
   fetchApplicationPrograms();
 });
-
 const fetchApplicationPrograms = async () => {
   const {
     data: { data: applicationPrograms, meta },
@@ -225,16 +196,13 @@ const fetchApplicationPrograms = async () => {
     limit: data.rowsPerPage,
     page: data.page,
   });
-
   data.applicationPrograms = applicationPrograms;
   data.totalCount = meta?.total_count;
   loading.value = false;
 };
-
 const changePage = (val) => {
   data.page = val;
   fetchApplicationPrograms();
 };
 </script>
-
 <style scoped src="./style.scss" />
