94e277ffc341e7fe84fb0291054d77c48f86c5a2
Rido Ananda <ridoananda123@gmail.com>
2023-01-30 18:33:31 +0700

    feat: navigation responsive in mobile view

--- src/layouts/DrawerLayout.vue
+++ src/layouts/DrawerLayout.vue
@@ -77,7 +77,7 @@
     </q-page-container>
     <q-page-sticky expand position="top">
       <q-toolbar style="background: #f5f5f5">
-        <MenuBar />
+        <MenuBar v-model="drawer" />
       </q-toolbar>
     </q-page-sticky>
     <q-page-sticky position="bottom-right" :offset="[18, 18]">
