2903388dc1912c25598adfe0f389a5a8d12a1cfb
Anugerah Christian Rompis <starleaf1@gmail.com>
2023-02-03 00:23:04 +0800

    Created interface for the message template.

---
+++ src/api/messageTemplateTypes.ts
@@ -0,0 +1,10 @@
+export interface MessageTemplateComponents {
+  components: object[];
+  created_by: string;
+  language: string;
+  messages_opened: number;
+  messages_sent: number;
+  name: string;
+  status: string;
+  top_block_reason: 0;
+}
