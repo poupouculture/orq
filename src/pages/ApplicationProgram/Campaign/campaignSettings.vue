16db659c7775da14221559b28602d4adcf137c12
andriferry <ferryandri35@gmail.com>
2023-03-07 10:50:46 +0700

    Process grid coloumn

--- src/pages/ApplicationProgram/Campaign/campaignSettings.vue
+++ src/pages/ApplicationProgram/Campaign/campaignSettings.vue
@@ -27,5 +27,21 @@
         <span>Campaign setting</span>
       </RouterLink>
     </div>
+
+    <div
+      class="grid mt-5 p-9 rounded-xl bg-white min-h-screen grid-cols-1 gap-5"
+    >
+      <div class="">
+        <p class="text-2xl font-semibold">Campaign Setting</p>
+      </div>
+
+      <div class="container">
+        <div class="grid grid-rows-1 lg:grid-rows-3 lg:grid-flow-col gap-4">
+          <div class="lg:row-span-3 bg-red-500">01</div>
+          <div class="lg:col-span-2 bg-red-500">02</div>
+          <div class="lg:row-span-2 lg:col-span-2 bg-red-500">03</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
