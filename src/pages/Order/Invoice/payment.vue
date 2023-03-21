ba110e2ebf0bd451c59b8d3a5415abb5062a554f
andriferry <ferryandri35@gmail.com>
2023-03-21 13:55:20 +0700

    update left side

--- src/pages/Order/Invoice/payment.vue
+++ src/pages/Order/Invoice/payment.vue
@@ -7,9 +7,11 @@ import Atome from "src/assets/images/atome.png";
 import Qr from "src/assets/images/qr.png";
 import AliPay from "src/assets/images/alipay.png";
 
-const payment = ref("creditCard");
 const { getInvoice, getCompany } = useInvoice();
 
+const payment = ref("creditCard");
+const firstTab = ref(true);
+const secondTab = ref(false);
 const paymentOptions = ref([
   {
     text: "Credit Card",
@@ -53,14 +55,27 @@ const paymentOptions = ref([
 
         <div>
           <div class="q-pa-md" style="max-width: 350px">
-            <q-list bordered>
+            <q-list>
               <q-expansion-item
                 group="somegroup"
-                icon="explore"
-                label="First"
-                default-opened
-                header-class="text-primary"
+                v-model="firstTab"
+                hide-expand-icon
               >
+                <template #header>
+                  <q-item-section avatar>
+                    <q-icon
+                      name="check_circle"
+                      :style="`color: ${
+                        firstTab ? '#4B44F6' : '#ccc'
+                      } ; font-size: 1.4em;`"
+                    />
+                  </q-item-section>
+
+                  <q-item-section class="capitalize font-semibold">
+                    request payment
+                  </q-item-section>
+                </template>
+
                 <q-card>
                   <q-card-section>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -75,48 +90,25 @@ const paymentOptions = ref([
 
               <q-expansion-item
                 group="somegroup"
-                icon="perm_identity"
-                label="Second"
-                header-class="text-teal"
+                v-model="secondTab"
+                hide-expand-icon
               >
+                <template #header>
+                  <q-item-section avatar>
+                    <q-icon
+                      name="check_circle"
+                      :style="`color: ${
+                        secondTab ? '#4B44F6' : '#ccc'
+                      } ; font-size: 1.4em;`"
+                    />
+                  </q-item-section>
+
+                  <q-item-section class="capitalize font-semibold">
+                    autocharge customer
+                  </q-item-section>
+                </template>
+
                 <q-card>
-                  <q-card-section>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Quidem, eius reprehenderit eos corrupti commodi magni
-                    quaerat ex numquam, dolorum officiis modi facere maiores
-                    architecto suscipit iste eveniet doloribus ullam aliquid.
-                  </q-card-section>
-                </q-card>
-              </q-expansion-item>
-
-              <q-separator />
-
-              <q-expansion-item
-                group="somegroup"
-                icon="shopping_cart"
-                label="Third"
-                header-class="text-purple"
-              >
-                <q-card>
-                  <q-card-section>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Quidem, eius reprehenderit eos corrupti commodi magni
-                    quaerat ex numquam, dolorum officiis modi facere maiores
-                    architecto suscipit iste eveniet doloribus ullam aliquid.
-                  </q-card-section>
-                </q-card>
-              </q-expansion-item>
-
-              <q-separator />
-
-              <q-expansion-item
-                group="somegroup"
-                icon="bluetooth"
-                label="Fourth"
-                header-class="bg-teal text-white"
-                expand-icon-class="text-white"
-              >
-                <q-card class="bg-teal-2">
                   <q-card-section>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                     Quidem, eius reprehenderit eos corrupti commodi magni
