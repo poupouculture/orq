d98a6932f5583e8039463e70d140900d72b034ea
andriferry <ferryandri35@gmail.com>
2023-04-07 05:33:09 +0700

    Update companies api

--- src/api/companies.ts
+++ src/api/companies.ts
@@ -2,7 +2,7 @@ import { api } from "boot/axios";
 
 export const getCompanies = async () => {
   const field =
-    "company_cd,company_number,name_english,name_chinese,customers,is_active";
+    "company_cd,company_number,name_english,name_chinese,customers.*,is_active";
   const result = await api.get(`/items/companies`, {
     params: {
       field,
