51be1475cbe42e6affd6ababa272c1dc52468246
andriferry <ferryandri35@gmail.com>
2023-04-21 20:06:25 +0700

    Start hero

---
+++ src/pages/landingpage/index.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Navbar from "./navbar.vue";
+</script>
+
+<template>
+  <div>
+    <div class="h-screen home flex flex-col items-center w-full p-10">
+      <div class="container">
+        <Navbar />
+
+        <div class="h-[85vh] flex flex-col justify-center items-start">
+          <div class="">
+            <p class="text-white capitalize font-black w-[80px] text-[70px]">
+              we provide business solution
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  background: linear-gradient(200.56deg, #40a4ff -3.58%, #e12179 94.64%),
+    #4b44f6;
+}
+</style>
---
+++ src/pages/landingpage/navbar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import logo from "assets/images/logo.svg";
+import menuIcon from "assets/images/menu.png";
+</script>
+
+<template>
+  <div class="navbar flex justify-between">
+    <div class="flex justify-center items-center gap-3">
+      <img class="w-[50px]" :src="logo" alt="logo" />
+      <p class="font-[800] text-white text-2xl">ChaQ</p>
+    </div>
+    <div class="flex items-center gap-5">
+      <button class="px-4 py-2 rounded-full bg-white/60 text-white">
+        Get started
+      </button>
+      <q-btn class="text-black" size="md" round color="white">
+        <img class="w-[20px]" :src="menuIcon" />
+      </q-btn>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
