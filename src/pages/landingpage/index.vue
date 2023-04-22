0c7b87237e5c66a8bbd5ad405280bd5ed6dbbc38
andriferry <ferryandri35@gmail.com>
2023-04-22 21:28:46 +0700

    Update home header

--- src/pages/landingpage/index.vue
+++ src/pages/landingpage/index.vue
@@ -4,15 +4,37 @@ import Navbar from "./navbar.vue";
 
 <template>
   <div>
-    <div class="h-screen home flex flex-col items-center w-full p-10">
+    <div class="min-h-screen home flex flex-col items-center w-full p-10">
       <div class="container">
         <Navbar />
 
-        <div class="h-[85vh] flex flex-col justify-center items-start">
-          <div class="">
+        <div class="min-h-[90vh] w-full flex flex-col mt-7 px-20 items-start">
+          <div class="w-100">
             <p class="text-white capitalize font-black w-[80px] text-[70px]">
               we provide business solution
             </p>
+
+            <div
+              class="flex flex-col mt-5 gap-5 text-white text-2xl font-normal w-4/5"
+            >
+              <span class="">
+                Streamline your workflow with our web app that includes
+                messaging.
+              </span>
+
+              <span class="">
+                Our goal is to provide a seamless and intuitive experience that
+                simplifies your business operations and builds strategic value
+                for your brand, whether you're in e-commerce, healthcare, or any
+                other industry.
+              </span>
+            </div>
+          </div>
+
+          <div class="flex w-full mt-24 justify-center">
+            <button class="rounded-full p-2 border-dashed border-2">
+              <q-icon class="text-white" name="arrow_downward" size="4rem" />
+            </button>
           </div>
         </div>
       </div>
