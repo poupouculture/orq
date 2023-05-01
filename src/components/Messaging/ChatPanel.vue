d3681ea8f94d90dd56a3cd1cfc67d4cb06583880
Rido Ananda <ridoananda123@gmail.com>
2023-05-01 08:44:30 +0700

    feat: notify when bot start and bot end

--- src/components/Messaging/ChatPanel.vue
+++ src/components/Messaging/ChatPanel.vue
@@ -101,10 +101,11 @@ import ChatList from "./ChatList.vue";
 import ChatListFooter from "./ChatListFooter.vue";
 import CustomerDialog from "./CustomerDialog.vue";
 import { IChat, SocketMessage } from "src/types/MessagingTypes";
-import { startNewChat } from "src/api/messaging";
+import { closeBot, startNewChat } from "src/api/messaging";
 import useUserInfoStore from "src/stores/modules/userInfo";
 import useCustomerStore from "src/stores/modules/customer";
 import { searchCustomers } from "src/api/customers";
+import { Notify } from "quasar";
 const rightDrawerOpen: any = inject("rightDrawerOpen");
 
 const ChatToggleLabel = {
@@ -140,7 +141,7 @@ const Tabs = reactive([
 ]);
 type ChatToggleType = {
   // eslint-disable-next-line prettier/prettier
-  state: typeof ChatToggleLabel[keyof typeof ChatToggleLabel];
+  state: (typeof ChatToggleLabel)[keyof typeof ChatToggleLabel];
 };
 const seachText = ref("");
 const userInfoStore = useUserInfoStore();
@@ -149,7 +150,7 @@ const chatToggleLabel: ChatToggleType = reactive({
   state: ChatToggleLabel.SHOW,
 });
 const messagingStore = useMessagingStore();
-const { chatsList, selectedTab, getSelectedChatId } =
+const { chatsList, selectedTab, getSelectedChatId, getSelectedChat } =
   storeToRefs(messagingStore);
 const showCustomerDialog = ref(false);
 const customerStore = useCustomerStore();
@@ -262,15 +263,27 @@ const initSocket = () => {
       });
 
       if (isConfirmed) {
-        const chat = (await onSearchCustomers(
+        const customer = (await onSearchCustomers(
           document?.summary?.customer_code,
           document?.summary?.location_code
         )) as any;
-        if (chat?.id) {
-          await customerStore.fetchCustomer(chat.id);
+        const chat = chatsList.value.find(
+          (chat) => chat.id === document.session_id
+        );
+        if (customer?.id && chat) {
+          messagingStore.onSelectChat(chat?.id);
+          await customerStore.fetchCustomer(customer.id);
           rightDrawerOpen.value = true;
         }
       }
+      await closeBot(getSelectedChatId.value);
+      Notify.create({
+        message: "The chatbot has been ended",
+        color: "blue-9",
+        position: "top",
+        type: "positive",
+      });
+      getSelectedChat.value.mode = "";
     });
   } catch (error) {
     console.log(error);
