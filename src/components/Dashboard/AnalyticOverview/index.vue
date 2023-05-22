55b753d0d9e9334f8451f935f079a08c3062f0f6
aldrichsancho <71678536+aldrichsancho@users.noreply.github.com>
2023-05-22 13:40:44 +0700

    fix:fix-dashbard-component

--- src/components/Dashboard/AnalyticOverview/index.vue
+++ src/components/Dashboard/AnalyticOverview/index.vue
@@ -63,7 +63,7 @@ const chatStatuses = [
 </script>
 
 <template>
-  <div class="w-full flex justify-between my-4">
+  <!-- <div class="w-full flex justify-between my-4">
     <span class="text-xl">Analytics Overview</span>
     <div class="flex p-2 bg-gray-200 rounded-md">
       <span class="text-gray-500"> Show: </span>
@@ -71,9 +71,9 @@ const chatStatuses = [
         <option value="">Last month</option>
       </select>
     </div>
-  </div>
+  </div> -->
 
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 mb-4">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 mb-4 mt-8">
     <SmallBox title="Current" title-color="text-primary" value="$2,545k" />
     <SmallBox title="Overdue" title-color="text-red-400" value="$3,356k" />
     <SmallBox title="Draft" title-color="text-gray-400" value="$30k" />
