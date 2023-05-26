b16cb8d993e1539af5414d2e549ea8ea5fd41d18
salimarizi <salim9asmp1pdl@gmail.com>
2023-05-26 21:02:07 +0700

    fix: fixing service

--- src/api/messaging.ts
+++ src/api/messaging.ts
@@ -13,8 +13,7 @@ export const getChats = async (type?: ChatTypes) => {
 
 export const getChatByID = async (id: string) => {
   const { data } = await api.get(`/chat/chats/${id}`);
-  console.log("get chat by id:", data);
-  return data.length > 0 ? data[0] : null;
+  return data.data.length > 0 ? data.data[0] : null;
 };
 
 export const getContactByChatId = async (id: string) => {
