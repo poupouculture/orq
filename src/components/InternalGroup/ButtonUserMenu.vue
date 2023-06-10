9f89ab21351a5482c832296a19efe90c22ab5e6d
andriferry <ferryandri35@gmail.com>
2023-06-10 14:49:54 +0700

    Update ButtonUsermenu

--- src/components/InternalGroup/ButtonUserMenu.vue
+++ src/components/InternalGroup/ButtonUserMenu.vue
@@ -1,3 +1,52 @@
+<script setup>
+import { Loading } from "quasar";
+import useCustomerStore from "src/stores/modules/customer";
+import useInternalGroup from "src/stores/modules/internalGroup";
+import { ref, computed } from "vue";
+// import BaseLayout from "../Customer/BaseLayout.vue";
+import DeleteDialog from "../Dialogs/DeleteDialog.vue";
+
+const customerStore = useCustomerStore();
+const internalGroupStore = useInternalGroup();
+
+// Props
+const props = defineProps({
+  id: [String, Number],
+  userId: [String, Number],
+  pagination: Object,
+  role: String,
+});
+
+// Ref
+const showUserDetail = ref(false);
+const deleteDialog = ref(false);
+
+// Computed
+const user = computed(() => customerStore.user);
+const pagination = computed(() => props.pagination);
+const userRole = computed(() => props.role);
+
+// Methods
+const openUserDetail = async () => {
+  Loading.show();
+  await customerStore.fetchUser(props.userId);
+  Loading.hide();
+  showUserDetail.value = true;
+};
+
+const submitDelete = async () => {
+  await internalGroupStore.deleteUser({
+    id: props.id,
+    userId: props.userId,
+  });
+
+  await internalGroupStore.getAll({
+    rowsPerPage: pagination.value.rowsPerPage,
+    page: pagination.value.page,
+  });
+};
+</script>
+
 <template>
   <q-btn color="grey-7" round flat icon="more_vert">
     <q-menu fit anchor="bottom middle" self="top right" auto-close>
@@ -16,164 +65,46 @@
       @submitDelete="submitDelete"
     />
   </q-btn>
-  <!-- Modal / Dialog Customer detail -->
-  <!-- <q-dialog v-model="showUserDetail">
-    <div
-      class="h-full bg-white w-auto !rounded-2xl px-7 py-4 overflow-y-auto relative"
-    >
-      <div class="cursor-pointer absolute top-6 right-7">
-        <svg
-          @click="showUserDetail = false"
-          width="14"
-          height="14"
-          viewBox="0 0 14 14"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M1 13L13 1M1 1L13 13"
-            stroke="#9CA3AF"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </div>
-      <BaseLayout mode="show" />
-    </div>
-  </q-dialog> -->
-  <!-- <q-dialog v-model="showUserDetail">
-    <div
-      class="h-full bg-white w-auto !rounded-2xl px-7 py-4 overflow-y-auto relative"
-    >
-      <div class="cursor-pointer absolute top-6 right-7">
-        <svg
-          @click="showUserDetail = false"
-          width="14"
-          height="14"
-          viewBox="0 0 14 14"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M1 13L13 1M1 1L13 13"
-            stroke="#9CA3AF"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </div>
-      <div class="mt-10">
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-4">
-          <div>
-            <p class="mb-2">Firstname</p>
-            <q-input
-              class="lg:w-full"
-              v-model="user.first_name"
-              outlined
-              lazy-rules
-              disable
-              dense
-            />
-          </div>
-          <div>
-            <p class="mb-2">Lastname</p>
-            <q-input
-              class="lg:w-full"
-              v-model="user.last_name"
-              outlined
-              lazy-rules
-              disable
-              dense
-            />
-          </div>
-        </div>
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-4">
-          <div>
-            <p class="mb-2">Email</p>
-            <q-input
-              class="lg:w-full"
-              v-model="user.email"
-              outlined
-              lazy-rules
-              disable
-              dense
-            />
-          </div>
-          <div>
-            <p class="mb-2">Status</p>
-            <q-input
-              class="lg:w-full"
-              v-model="user.status"
-              outlined
-              lazy-rules
-              disable
-              dense
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  </q-dialog> -->
 
+  <!-- Modal / Dialog Customer detail -->
   <q-dialog v-model="showUserDetail">
     <q-card class="w-5/12">
-      <q-card-section>
-        <div class="text-h6">Alert</div>
-      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn flat round icon="close" v-close-popup />
+      </q-card-actions>
 
       <q-card-section class="q-pt-none">
-        aaa {{ props.userData }}
-      </q-card-section>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-4">
+          <div class="col-span-2 lg:col-span-1">
+            <p class="mb-2">Firstname</p>
+            <q-input dense outlined disable v-model="user.first_name" />
+          </div>
 
-      <q-card-actions align="right">
-        <q-btn flat label="OK" color="primary" v-close-popup />
-      </q-card-actions>
+          <div class="col-span-2 lg:col-span-1">
+            <p class="mb-2">Lastname</p>
+            <q-input dense outlined disable v-model="user.last_name" />
+          </div>
+
+          <div class="col-span-2 lg:col-span-1">
+            <p class="mb-2">Email</p>
+            <q-input dense outlined disable v-model="user.email" />
+          </div>
+
+          <div class="col-span-2 lg:col-span-1">
+            <p class="mb-2">Status</p>
+            <q-input dense outlined disable v-model="user.status" />
+          </div>
+
+          <div v-if="userRole" class="col-span-2 lg:col-span-1">
+            <p class="mb-2">Role</p>
+            <q-input dense outlined disable v-model="userRole" />
+          </div>
+        </div>
+      </q-card-section>
     </q-card>
   </q-dialog>
 </template>
-<script setup>
-import { Loading } from "quasar";
-import useInternalGroup from "src/stores/modules/internalGroup";
-import { ref, computed } from "vue";
-// import BaseLayout from "../Customer/BaseLayout.vue";
-import DeleteDialog from "../Dialogs/DeleteDialog.vue";
-const props = defineProps({
-  id: [String, Number],
-  userId: [String, Number],
-  pagination: Object,
-  userData: Object,
-});
 
-// const customerStore = useCustomerStore();
-// const user = computed(() => customerStore.user);
-
-const internalGroupStore = useInternalGroup();
-const showUserDetail = ref(false);
-const deleteDialog = ref(false);
-
-const pagination = computed(() => props.pagination);
-
-// Methods
-const openUserDetail = async () => {
-  Loading.show();
-  Loading.hide();
-  showUserDetail.value = true;
-};
-
-const submitDelete = async () => {
-  await internalGroupStore.deleteUser({
-    id: props.id,
-    userId: props.userId,
-  });
-
-  await internalGroupStore.getAll({
-    rowsPerPage: pagination.value.rowsPerPage,
-    page: pagination.value.page,
-  });
-};
-</script>
 <style lang="scss" scoped>
 .date {
   position: relative;
