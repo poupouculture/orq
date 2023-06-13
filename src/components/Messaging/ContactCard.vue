b13fd7b3cf88be5c600c1eb549e5e3835788e13c
ridoananda <ridoananda123@gmail.com>
2023-06-14 06:49:59 +0700

    feat: time contact card in messaging

--- src/components/Messaging/ContactCard.vue
+++ src/components/Messaging/ContactCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     :class="{ 'bg-primary': active }"
-    class="flex w-full contact-card q-pa-sm"
+    class="flex w-full contact-card q-pa-sm relative"
   >
     <q-avatar class="rounded-avatar q-mr-sm">
       <img :src="profileIcon" />
@@ -27,18 +27,45 @@
         color="red"
         :label="props.data.totalUnread"
       />
+      <div
+        class="absolute bottom-2 flex items-center text-xs gap-x-1"
+        :class="[{ '-bottom-0.5': props.data.totalUnread }, waitingTimeStatus]"
+        v-if="selectedTab === ChatTypes.PENDING"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="12"
+          height="12"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path
+            d="M6 20v-2a6 6 0 1 1 12 0v2a1 1 0 0 1 -1 1h-10a1 1 0 0 1 -1 -1z"
+          ></path>
+          <path
+            d="M6 4v2a6 6 0 1 0 12 0v-2a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1z"
+          ></path>
+        </svg>
+        {{ waitingTime }}
+      </div>
     </q-item-section>
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
-import { format } from "date-fns";
+import { differenceInMinutes, format, intlFormatDistance } from "date-fns";
 import { storeToRefs } from "pinia";
 import { IChat, MessageType } from "src/types/MessagingTypes";
 import useMessagingStore from "src/stores/modules/messaging";
 import profileIcon from "src/assets/images/profileicon.svg";
 import { getChatNameEn, getContactNameEn } from "src/utils/trim-word";
+import { ChatTypes } from "src/constants/ChatKeyword";
 
 export interface Props {
   data?: IChat;
@@ -56,7 +83,7 @@ const props = withDefaults(defineProps<Props>(), {
   data: () => ({} as IChat),
 });
 const messagingStore = useMessagingStore();
-const { selectedChatId } = storeToRefs(messagingStore);
+const { selectedChatId, selectedTab } = storeToRefs(messagingStore);
 
 const active = computed<boolean>(() => props.data.id === selectedChatId.value);
 const name = computed(() => getChatNameEn(props.data));
@@ -98,9 +125,36 @@ const time = computed<string>(() => {
   const dateCreated = lastMessage?.date_created;
   return dateCreated && format(new Date(dateCreated), "p, d LLL");
 });
+const waitingTime = computed(() => {
+  const { last_message: lastMessage } = props.data;
+  const dateCreated = lastMessage?.date_created;
+  return intlFormatDistance(new Date(dateCreated), new Date());
+});
+const waitingTimeStatus = computed(() => {
+  const { last_message: lastMessage } = props.data;
+  const dateCreated = lastMessage?.date_created;
+  const minutes = differenceInMinutes(new Date(), new Date(dateCreated));
+  switch (true) {
+    case minutes > 60 && minutes < 120:
+      return "orange-status";
+    case minutes > 120:
+      return "red-status";
+    default:
+      return "green-status";
+  }
+});
 </script>
 
 <style scoped lang="scss">
+.green-status {
+  @apply text-green-500;
+}
+.orange-status {
+  @apply text-orange-400;
+}
+.red-status {
+  @apply text-red-600;
+}
 .rounded-avatar {
   border-radius: 50% !important;
 }
