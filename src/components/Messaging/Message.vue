f4808ac4b942fc7a3c615db7f06f766b88c0f459
salimarizi <salim9asmp1pdl@gmail.com>
2023-06-14 22:03:56 +0700

    fix: fixing duplicate file upload

--- src/components/Messaging/Message.vue
+++ src/components/Messaging/Message.vue
@@ -995,6 +995,13 @@ const uploadFile = async (files: readonly File[]) => {
   fileUplader.value?.removeQueuedFiles();
 
   const { data } = await uploadMedia(getSelectedChatId.value, bodyFormData);
+
+  // clear uploaded file from cache
+  cachedChatMessages.value[getSelectedChatId.value] = cachedChatMessages.value[
+    getSelectedChatId.value
+  ].filter((cm) => cm.id !== newMessage.id);
+  // end clear uploaded file from cache
+
   messageCallback(data, newMessage);
 };
 
