988be4ea742a6145d937fc57951fd3f7e1bdd434
Anugerah Christian Rompis <starleaf1@gmail.com>
2023-07-13 23:04:19 +0800

    Adjusted layout for mobile and when buttons are shown/hidden.

--- src/components/Messaging/ChatMembersBar.vue
+++ src/components/Messaging/ChatMembersBar.vue
@@ -1,28 +1,70 @@
 <template>
-  <q-toolbar class="items-center pb-3">
-    <div class="text-gray-400">Members</div>
-    <div class="flex justify-between p-2 items-center">
-      <div class="flex">
+  <div class="items-center p-3">
+    <div class="flex row items-center justify-between">
+      <div
+        class="col-grow flex justify-between"
+        :class="{
+          row: showChatOption,
+          column: !showChatOption,
+          'items-center': showChatOption,
+          'items-start': !showChatOption,
+        }"
+      >
+        <div class="text-gray-400">Members</div>
         <div
-          class="w-10 h-10 flex justify-center mr-2 items-center rounded-full bg-gray-200"
-          v-for="(member, index) of members.slice(0, 3)"
-          :key="index"
+          class="flex justify-between py-2 items-center col-grow"
+          :class="{ 'ml-3': showChatOption, 'full-width': !showChatOption }"
         >
-          {{ initialName(member?.name) }}
-        </div>
-        <div
-          class="w-10 h-10 flex justify-center mr-2 items-center rounded-full bg-gray-300"
-          v-if="members.length > 3"
-        >
-          {{ members.length - 3 }} +
+          <div class="flex">
+            <div
+              class="w-10 h-10 flex justify-center mr-1 items-center rounded-full bg-gray-200"
+              v-for="(member, index) of members.slice(0, 3)"
+              :key="index"
+            >
+              {{ initialName(member?.name) }}
+            </div>
+            <div
+              class="w-10 h-10 flex justify-center mr-2 items-center rounded-full bg-gray-300"
+              v-if="members.length > 3"
+            >
+              {{ members.length - 3 }} +
+            </div>
+          </div>
+          <q-space />
+          <q-btn
+            v-if="selectedChat?.status !== ChatTypes.CLOSED && !showChatOption"
+            flat
+            padding="0"
+            color="primary"
+            @click="$emit('update:showChatOption', true)"
+            >Show Actions</q-btn
+          >
         </div>
       </div>
     </div>
-  </q-toolbar>
+  </div>
+  <div v-if="selectedChat?.status !== ChatTypes.CLOSED">
+    <div
+      v-if="showChatOption"
+      class="p-3 flex justify-between items-center row"
+    >
+      <ChatConversationButton />
+      <q-btn
+        padding="none"
+        flat
+        color="primary"
+        @click="$emit('update:showChatOption', false)"
+      >
+        Hide Actions
+      </q-btn>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ChatTypes } from "src/constants/ChatKeyword";
 import { toRefs } from "vue";
+import ChatConversationButton from "./ChatConversationButton.vue";
 
 const props = defineProps({
   members: {
@@ -42,7 +84,9 @@ const props = defineProps({
   },
 });
 
-const { members } = toRefs(props);
+defineEmits(["update:showChatOption"]);
+
+const { members, showChatOption, selectedChat } = toRefs(props);
 
 const initialName = (name: string) => {
   const firstName = name.split(" ")[0];
