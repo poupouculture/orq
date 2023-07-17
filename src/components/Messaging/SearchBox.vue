20d273ba6be746cf0bee7b90357b5c5375b34ebc
Anugerah Christian Rompis <starleaf1@gmail.com>
2023-07-18 02:11:37 +0800

    Implemented the new layout.

---
+++ src/components/Messaging/SearchBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-input
+    class="self-center mr-3"
+    :class="{ 'mb-3': !isMobile, 'mb-1': isMobile }"
+    dense
+    clearable
+    outlined
+    label="Search"
+    v-bind="$props"
+    input-style="max-width: 125px"
+  >
+    <template v-slot:prepend>
+      <q-icon name="search" />
+    </template>
+  </q-input>
+</template>
+
+<script setup>
+import { computed } from "vue";
+const isMobile = computed(() => window.innerWidth < 768);
+</script>
