9d19063f2ff8b36b66c6a0eabbba45cf2549af9c
Anugerah Christian Rompis <starleaf1@gmail.com>
2023-07-22 20:11:13 +0800

    Implemented the search component.

--- src/components/Messaging/SearchBox.vue
+++ src/components/Messaging/SearchBox.vue
@@ -8,13 +8,26 @@
     outlined
     label="Search"
     v-bind="$props"
+    v-model="internalValue"
+    :options="options"
     input-style="max-width: 125px"
+    option-value="id"
+    option-label="label"
     use-input
     @filter="filterFn"
     :input-debounce="250"
+    behavior="dialog"
   >
     <template v-slot:prepend>
       <q-icon name="search" />
+    </template>
+    <template v-slot:option="scope">
+      <q-item v-bind="scope.itemProps">
+        <q-item-section>
+          <q-item-label caption>{{ scope.opt.dateCreated }}</q-item-label>
+          <q-item-label>{{ scope.opt.label }}</q-item-label>
+        </q-item-section>
+      </q-item>
     </template>
   </q-select>
 </template>
@@ -25,24 +38,39 @@ import { Platform } from "quasar";
 import { searchMessages } from "src/api/messaging";
 import useMessagingStore from "src/stores/modules/messaging";
 import useUserInfoStore from "src/stores/modules/userInfo";
+import { format } from "date-fns";
 
 const messagingStore = useMessagingStore();
 const userInfoStore = useUserInfoStore();
 const isMobile = computed(() => Platform.is.mobile);
 
+const internalValue = ref(null);
+
 const options = ref([]);
-const filterFn = (value) => {
-  try {
-    if (value === "") {
+const filterFn = async (value, update, abort) => {
+  if (value === "") {
+    update(() => {
       options.value = [];
-    }
-    options.value = searchMessages({
+    });
+    return;
+  }
+  try {
+    const { data } = await searchMessages({
       keyword: value,
       chatId: messagingStore.getSelectedChatId,
       userId: userInfoStore.getUserId,
     });
+    update(() => {
+      // eslint-disable-next-line camelcase
+      options.value = data.map(({ id, content, date_created }) => ({
+        id,
+        label: content.text,
+        dateCreated: format(new Date(date_created), "p, d MMM"),
+      }));
+    });
   } catch (e) {
-    console.error("[search-message] Error in component", e);
+    abort();
+    console.error(e);
   }
 };
 </script>
