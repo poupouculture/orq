5177f381ae302c94567e6ae23b06c358a629b9a1
Anugerah Christian Rompis <starleaf1@gmail.com>
2023-07-22 20:10:36 +0800

    Fixed the credentials for search message API.

--- src/api/messaging.ts
+++ src/api/messaging.ts
@@ -343,7 +343,6 @@ export const searchMessages = async ({ chatId, userId, keyword }) => {
     `/search/messages/${encodedKeyword}/${chatId}/${userId}`
   );
 
-  console.log("[search-messages] result", result);
   const { data } = result;
   return data;
 };
