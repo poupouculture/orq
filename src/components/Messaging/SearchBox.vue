f23cb14a6ccd76643b0e9f77f17eaf6dcade80eb
Anugerah Christian Rompis <starleaf1@gmail.com>
2023-07-22 12:59:17 +0800

    Implemented API call in search box component.

--- src/components/Messaging/SearchBox.vue
+++ src/components/Messaging/SearchBox.vue
@@ -1,5 +1,6 @@
 <template>
-  <q-input
+  <q-select
+    autocomplete
     class="self-center mr-3"
     :class="{ 'mb-3': !isMobile, 'mb-1': isMobile }"
     dense
@@ -8,14 +9,40 @@
     label="Search"
     v-bind="$props"
     input-style="max-width: 125px"
+    use-input
+    @filter="filterFn"
+    :input-debounce="250"
   >
     <template v-slot:prepend>
       <q-icon name="search" />
     </template>
-  </q-input>
+  </q-select>
 </template>
 
 <script setup>
-import { computed } from "vue";
-const isMobile = computed(() => window.innerWidth < 768);
+import { computed, ref } from "vue";
+import { Platform } from "quasar";
+import { searchMessages } from "src/api/messaging";
+import useMessagingStore from "src/stores/modules/messaging";
+import useUserInfoStore from "src/stores/modules/userInfo";
+
+const messagingStore = useMessagingStore();
+const userInfoStore = useUserInfoStore();
+const isMobile = computed(() => Platform.is.mobile);
+
+const options = ref([]);
+const filterFn = (value) => {
+  try {
+    if (value === "") {
+      options.value = [];
+    }
+    options.value = searchMessages({
+      keyword: value,
+      chatId: messagingStore.getSelectedChatId,
+      userId: userInfoStore.getUserId,
+    });
+  } catch (e) {
+    console.error("[search-message] Error in component", e);
+  }
+};
 </script>
