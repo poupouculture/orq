34e2ad9c79a9a3ee0ebf69a81dac662460d27bc7
Anugerah Christian Rompis <starleaf1@gmail.com>
2023-07-27 07:52:30 +0800

    Conditionally create the socketApi

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -23,10 +23,11 @@ const api = axios.create({
   baseURL: process.env.BACKEND_URL,
   withCredentials: true,
 });
+
 const socketApi = axios.create({
   baseURL: process.env.SOCKETS_URL,
   headers: {
-    Authorization: userinfo.access_token,
+    Authorization: userinfo?.access_token,
   },
   withCredentials: false,
 });
