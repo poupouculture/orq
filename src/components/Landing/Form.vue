f56757660e67c9483e8326e67730cba1863193ac
andriferry <ferryandri35@gmail.com>
2023-08-10 16:55:46 +0700

    Update form component

--- src/components/Landing/Form.vue
+++ src/components/Landing/Form.vue
@@ -4,21 +4,24 @@ defineProps({
     type: Object,
   },
 });
-
-const alignmentClass = (classValue) => {
-  if (classValue === "center") {
-    return "justify-center";
-  } else if (classValue === "left") {
-    return "justify-start";
-  } else if (classValue === "right") {
-    return "justify-end";
-  }
-};
 </script>
 
 <template>
-  <div :class="alignmentClass(content.alignment)" class="w-full mx-7 my-5 flex">
-    <div class="flex gap-5 flex-col w-full lg:w-[700px]">
+  <div
+    class="w-full mx-7 my-5 grid gap-5 lg:grid-cols-2 bg-center bg-cover p-2 lg:p-6"
+    :style="{ backgroundImage: `url(${content.image})` }"
+  >
+    <div
+      :class="content.alignment === 'left' ? 'order-1' : 'order-2'"
+      class="col-span-1 text-white"
+    >
+      <article v-html="content.content" class="prose text-white" />
+    </div>
+
+    <div
+      :class="content.alignment === 'right' ? 'order-2' : 'order-1'"
+      class="flex bg-white p-6 rounded-lg gap-5 col-span-1 flex-col w-full"
+    >
       <p class="text-center mb-4 font-bold text-xl">
         {{ content.name }}
       </p>
@@ -28,20 +31,21 @@ const alignmentClass = (classValue) => {
         :key="index"
         class="grid grid-cols-6"
       >
-        <div class="col-span-2">
+        <div class="col-span-6 lg:col-span-2">
           <p class="label-style mb-2">
-            {{ form.label }} <span class="text-red-600">*</span>
+            {{ form.label }}
+            <span v-if="form.required" class="text-red-600">*</span>
           </p>
         </div>
 
-        <div class="col-span-4">
+        <div class="col-span-6 lg:col-span-4">
           <q-input
             v-model="form.value"
             :type="form.type"
             outlined
             lazy-rules
             dense
-          ></q-input>
+          />
         </div>
       </div>
 
