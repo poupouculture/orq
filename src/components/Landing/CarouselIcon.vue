4d09c069f1d840a9c0eca61d7c07b01f67817f9c
ridoananda <ridoananda123@gmail.com>
2023-08-14 10:39:09 +0700

    fix(landing): carousel icon component

--- src/components/Landing/CarouselIcon.vue
+++ src/components/Landing/CarouselIcon.vue
@@ -1,4 +1,10 @@
 <script setup>
+import { Vue3Marquee } from "vue3-marquee";
+// const imgArray = [
+//   "https://sponsors.vuejs.org/images/vueschool.avif",
+//   "https://sponsors.vuejs.org/images/vehikl.avif",
+//   "https://sponsors.vuejs.org/images/dronahq.avif",
+// ];
 defineProps({
   content: {
     type: Object,
@@ -7,33 +13,46 @@ defineProps({
 </script>
 
 <template>
-  <div class="w-full h-32 relative items-center">
-    <div
-      class="relative h-32 w-full overflow-x-hidden top-0 no-wrap left-0 flex gap-[100px] py-2"
+  <Vue3Marquee direction="reverse" :duration="5">
+    <img
+      v-for="(img, i) in content.children"
+      :key="i"
+      :src="img.icon"
+      class="w-32 h-32 object-cover mx-6"
+    />
+  </Vue3Marquee>
+  <!-- <div class="w-full h-32 relative items-center overflow-x-hidden"> -->
+  <!-- <div
+      ref="items"
+      class="clientsItem relative max-h-40 w-full overflow-x-hidden gap-x-4 top-0 no-wrap left-0 flex lg:gap-[100px] py-2"
     >
       <q-img
         v-for="(image, index) in content.children"
         :key="index"
-        class="rounded-borders clientsItem"
+        class="rounded-borders"
         :src="image.icon"
         no-spinner
         style="max-width: 150px"
       />
-    </div>
-  </div>
+    </div> -->
+  <!-- </div> -->
 </template>
 
 <style scoped lang="scss">
 @keyframes moveAcross {
   0% {
-    transform: translateX(-1800px);
+    transform: translateX(-100%);
   }
   100% {
-    transform: translateX(1800px);
+    transform: translateX(100%);
   }
 }
 
 .clientsItem {
-  animation: moveAcross 15s linear infinite;
+  animation: moveAcross 10s linear infinite;
+}
+.clientsItem2 {
+  animation: moveAcross 10s linear infinite;
+  animation-delay: 6000ms;
 }
 </style>
