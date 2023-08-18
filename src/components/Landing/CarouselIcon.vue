0ece049cda9e0bff99155e79716196643abb0f61
andriferry <ferryandri35@gmail.com>
2023-08-18 17:39:56 +0700

    Update carousel conditional

--- src/components/Landing/CarouselIcon.vue
+++ src/components/Landing/CarouselIcon.vue
@@ -6,20 +6,63 @@ defineProps({
     type: Object,
   },
 });
+
+const flexDirection = (alignment) => {
+  if (alignment === "center") {
+    return "justify-center";
+  } else if (alignment === "left") {
+    return "justify-start";
+  } else if (alignment === "right") {
+    return "justify-end";
+  }
+};
 </script>
 
 <template>
-  <Vue3Marquee direction="reverse" :duration="5">
-    <img
-      v-for="(img, i) in content.children"
-      :key="i"
-      :src="img.icon"
-      width="150"
-      height="150"
-      class="object-cover mx-6"
-    />
-  </Vue3Marquee>
-  <div class=""></div>
+  <div class="w-full py-6">
+    <div class="w-full flex justify-center">
+      <article v-html="content.content" class="prose"></article>
+    </div>
+
+    <template v-if="content?.raw !== null">
+      <div
+        v-if="content?.raw.style === 'grid'"
+        class="w-full flex"
+        :class="flexDirection(content.alignment)"
+      >
+        <div class="flex justify-center">
+          <div class="flex flex-wrap justify-center">
+            <div
+              class="flex items-center"
+              v-for="(img, i) in content.children"
+              :key="i"
+            >
+              <img
+                :src="img.icon"
+                width="150"
+                height="150"
+                class="object-cover mx-6"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Vue3Marquee
+        v-if="content?.raw.style === 'carousel'"
+        v-bind="content?.raw.props"
+      >
+        <img
+          v-for="(img, i) in content.children"
+          :key="i"
+          :src="img.icon"
+          width="150"
+          height="150"
+          class="object-cover mx-6"
+        />
+      </Vue3Marquee>
+    </template>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
