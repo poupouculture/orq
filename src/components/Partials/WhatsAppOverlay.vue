caaf91fe6df74b090d9837026f563651b6da9cd3
andriferry <ferryandri35@gmail.com>
2023-08-19 12:55:04 +0700

    Update rebase

---
+++ src/components/Partials/WhatsAppOverlay.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  data: any;
+}>();
+</script>
+<template>
+  <div class="fixed bottom-5 right-7 z-[99999999]">
+    <a :href="`https://wa.me/${data.raw.number}`" target="_blank">
+      <img src="../../assets/icons/whatsapp.svg" class="w-20 h-20" />
+    </a>
+  </div>
+</template>
