a4b7293d4694f23891dceb580c177cdc87a02095
Christian Rompis <christian.rompis@voicefoundry.com.au>
2023-08-23 23:20:32 +0800

    Added 2 client logos.

--- src/components/Landing/section6.vue
+++ src/components/Landing/section6.vue
@@ -2,10 +2,12 @@
 import angliss from "src/assets/images/angliss.png";
 import tasteofasia from "src/assets/images/tasteofasia.png";
 import fitmax from "src/assets/images/fitmax.png";
+import paradise from "src/assets/images/paradise.jpeg";
+import ks from "src/assets/images/k.jpeg";
 
 import { ref } from "vue";
 
-const clients = ref([angliss, fitmax, tasteofasia]);
+const clients = ref([angliss, fitmax, tasteofasia, paradise, ks]);
 </script>
 
 <template>
@@ -22,13 +24,13 @@ const clients = ref([angliss, fitmax, tasteofasia]);
       </p>
     </div>
 
-    <div class="grid grid-cols-1 mt-10 sm:grid-cols-3 gap-16">
+    <div class="grid grid-cols-1 mt-10 sm:grid-cols-3 md:grid-cols-5 gap-16">
       <div
         v-for="(client, index) in clients"
         :key="index"
         class="flex justify-center"
       >
-        <img :src="client" class="w-32" alt="" />
+        <img :src="client" class="w-32" alt="" style="object-fit: contain" />
       </div>
     </div>
   </div>
