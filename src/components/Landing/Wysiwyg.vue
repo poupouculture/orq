5ab7b7cc2cabe4e7d78f4c03f423e0a57980a9de
Anugerah Christian Rompis <starleaf1@gmail.com>
2023-08-27 20:47:29 +0800

    Revert "Undo changes."
    This reverts commit fc95640fc4725f4fcea1a8efbc03a6f13c076405.

--- src/components/Landing/Wysiwyg.vue
+++ src/components/Landing/Wysiwyg.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { useQuasar } from "quasar";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 
 const props = defineProps({
   content: {
@@ -8,29 +7,20 @@ const props = defineProps({
   },
 });
 
-const $q = useQuasar();
+const defaultStyle = ref({
+  padding: "20px",
+});
 
-// const defaultStyle = ref({
-//   padding: "20px",
-// });
-
-// const style = computed(() => {
-//   return props.content?.raw !== null
-//     ? props.content?.raw.style
-//     : defaultStyle.value;
-// });
-
-const displayedContent = computed(
-  () =>
-    ($q.platform.is.mobile
-      ? props.content?.content_mobile ?? props.content?.content
-      : props.content?.content) ?? null
-);
+const style = computed(() => {
+  return props.content?.raw !== null
+    ? props.content?.raw.style
+    : defaultStyle.value;
+});
 </script>
 
 <template>
-  <div>
-    <article v-html="displayedContent" class="prose max-w-none" />
+  <div :style="style">
+    <article v-html="props.content?.content" class="prose max-w-none" />
   </div>
 </template>
 
