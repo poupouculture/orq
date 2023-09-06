5f6299c6ce29ab86bb0fd9c21c9b4c6e142cb391
ridoananda <ridoananda123@gmail.com>
2023-09-06 10:48:11 +0700

    fix: cover photo responsive content and alignment

--- src/components/Landing/Banner.vue
+++ src/components/Landing/Banner.vue
@@ -9,12 +9,21 @@ import CarouselIcon from "./CarouselIcon.vue";
 import Form from "./Form.vue";
 import TextWithImageAbove from "./TextWithImageAbove.vue";
 import BackgroundWithIcon from "./Carousel/BackgroundWithIcon.vue";
+import { useQuasar } from "quasar";
 
 const props = defineProps({
   content: {
     type: Object,
   },
 });
+
+const $q = useQuasar();
+
+const displayedContent = (data) => {
+  return $q.platform.is.mobile
+    ? data?.content_mobile ?? data?.content_mobile
+    : data?.content;
+};
 
 const flexDirection = (alignment) => {
   if (alignment === "center") {
@@ -23,6 +32,8 @@ const flexDirection = (alignment) => {
     return "justify-start";
   } else if (alignment === "right") {
     return "justify-end";
+  } else if (alignment === "above") {
+    return "justify-start items-start";
   }
 };
 </script>
@@ -39,20 +50,15 @@ const flexDirection = (alignment) => {
       </div>
 
       <Cover v-else-if="data.type === 'cover_photo'" :content="data">
-        <div class="w-full">
+        <div class="w-full h-full">
           <div
-            class="flex capitalize gap-3 lg:gap-0"
+            class="flex capitalize gap-3 lg:gap-0 h-full w-full"
             :class="flexDirection(data.alignment)"
           >
             <article
-              v-html="data.content_mobile"
-              class="prose !block md:!hidden"
-            ></article>
-
-            <article
-              v-html="data.content"
-              class="prose !hidden md:!block"
-            ></article>
+              v-html="displayedContent(data)"
+              class="prose max-w-none prose-img:m-0"
+            />
           </div>
         </div>
       </Cover>
